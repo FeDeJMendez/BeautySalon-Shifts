970ca89a33edcbda63846c3f7177d4ddd4fdd9ee
FeDeJMendez <fedemendezg@gmail.com>
2021-07-26 21:54:38 -0300

    Init

---
+++ src/main/java/com/CodingDesign/BeautySalonShifts/BeautySalonShiftsApplication.java
@@ -0,0 +1,13 @@
+package com.CodingDesign.BeautySalonShifts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautySalonShiftsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautySalonShiftsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CodingDesign/BeautySalonShifts/BeautySalonShiftsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CodingDesign.BeautySalonShifts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautySalonShiftsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
